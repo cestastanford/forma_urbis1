64fd0acff59b9ad7cbeee9f50fc41b11b8abdc8f
codyml <codymleff@gmail.com>
2015-07-09 19:42:42 -0700

    Finished layerListView.js.

--- js/layerListView.js
+++ js/layerListView.js
@@ -11,10 +11,8 @@ var LayerListView = function(layers) {
     /*
     *   Handlebars compile functions for the list elements.
     */
-    var rasterLayerTemplate = $('#raster-layer-template')[0];
-    var renderRasterLayer = Handlebars.compile(rasterLayerTemplate.innerHTML);
-//    var vectorLayerTemplate = $('#vector-layer-template')[0];
-//    var renderVectorLayer = Handlebars.compile(vectorLayerTemplate)[0];
+    var layerTemplate = $('#layer-template')[0];
+    var renderLayer = Handlebars.compile(layerTemplate.innerHTML);
 
     /*
     *   Instance variable for the list elements.
@@ -22,15 +20,26 @@ var LayerListView = function(layers) {
     this.$list = $('#layer-list .list');
 
     /*
-    *   Populates the list with raster layers, creating an element
-    *   with a checkbox and an opacity slider for each.
+    *   Populates the list with layers, creating an element
+    *   with a checkbox for each.
     */
     for (var i = 0; i < layers.raster.length; i++) {
-        var $rasterElement = $(renderRasterLayer({
+        var $rasterElement = $(renderLayer({
             name: layers.raster[i].name,
             type: 'raster',
-            index: i
+            index: i,
+            raster: true
         }));
         this.$list.append($rasterElement);
     }
+    for (var i = 0; i < layers.vector.length; i++) {
+        var $vectorElement = $(renderLayer({
+            name: layers.raster[i].name,
+            type: 'raster',
+            index: i,
+            raster: false
+        }));
+        this.$list.append($vectorElement);
+    }
+
 }
